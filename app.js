21f759978af76d1b5f370ba44601aba8a1f874f5
Hector Ivan <gho0thubun@gmail.com>
2018-07-20 16:34:26 -0500

    Se completan las demás temperaturas

--- app.js
+++ app.js
@@ -12,10 +12,29 @@ function celTransfrom(){
     let celTemp = parseFloat(inputCelsius.value);
     let faranTemp = (celTemp * (9/5)) + 32;
     let kelTemp = celTemp + 273.5;
-    console.log(celTemp.value);
 
     inputFaraheit.value = roundNum(faranTemp);
     inputKelvin.value = roundNum(kelTemp);
 };
+function farTransform(){
+    let farTemp = parseFloat(inputFaraheit.value);
+    let celTemp = (farTemp - 32 )* (5/9);
+    let kelTemp = (farTemp + 469.57) * (5/9);
+    inputCelsius.value = roundNum(celTemp);
+    inputKelvin.value = roundNum(kelTemp);
+};
+function kelTransfrom(){
+    let kelTemp = parseFloat(inputKelvin.value);
+    let faranTemp = (9/5) * ( kelTemp - 273) +32 ;
+    let celTemp = kelTemp - 273.15;
+    inputCelsius.value = roundNum(celTemp);
+    inputFaraheit.value = roundNum(faranTemp);
+};
 
-inputCelsius.addEventListener('input', celTransfrom);
+function main()
+{
+    inputCelsius.addEventListener('input', celTransfrom);
+    inputFaraheit.addEventListener('input',farTransform);
+    inputKelvin.addEventListener('input',kelTransfrom);
+}
+main();
